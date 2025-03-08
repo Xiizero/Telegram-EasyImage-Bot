2e0c2852543cf4bfd260a37b72f4e339b40c45f7
BreezeZhang <135970538+Xiizero@users.noreply.github.com>
2025-03-08 17:14:50 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# 配置文件
+import os
+
+# Telegram 配置
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+# EasyImages2.0 API 配置
+EASY_IMAGES_API_URL = os.getenv("EASY_IMAGES_API_URL")  # EasyImages2.0 后端地址
+EASY_IMAGES_API_KEY = os.getenv("EASY_IMAGES_API_KEY")  # API 密钥
